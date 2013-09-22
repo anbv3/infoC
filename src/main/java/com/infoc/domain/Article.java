253f8e435b3ef5dd2226987e2e865cf8574e0663
NB10999 <sanghun.an@nhn.com>
2013-09-22 18:01:02 +0900

    코드 정리

--- src/main/java/com/infoc/domain/Article.java
+++ src/main/java/com/infoc/domain/Article.java
@@ -7,7 +7,9 @@ import java.util.Set;
 
 import org.joda.time.DateTime;
 
+import com.google.common.base.Splitter;
 import com.google.common.base.Strings;
+import com.google.common.collect.Lists;
 import com.google.common.collect.Ordering;
 import com.google.common.collect.Sets;
 import com.infoc.service.CollectionService;
@@ -34,16 +36,8 @@ public class Article {
 	private Set<String> keyWordList = new HashSet<>();
 
 	private List<SentenceInfo> sentenceList = new ArrayList<>();
-
-	public void createKeyWorkList() {
-		if (Strings.isNullOrEmpty(this.title)) {
-			return;
-		}
-		
-		this.keyWordList = Sets.newHashSet(CollectionService.SPLITTER.split(this.title));
-		
-		
-	}
+	
+	private List<SentenceInfo> keySentenceList = new ArrayList<>();
 
 	public static final Ordering<Article> dateOrdering = new Ordering<Article>() {
 		@Override
@@ -52,14 +46,55 @@ public class Article {
 		}
 	};
 
-	private void omitSpecialChar(String str) {
+	public void createKeyWorkList() {
+		if (Strings.isNullOrEmpty(this.title)) {
+			return;
+		}
+		
+		Set<String> titleList = Sets.newHashSet(CollectionService.SPLITTER.split(this.title));
+		for (String word : titleList) {
+			if (word.length() > 1 && !isSpecialChar(word)) {
+				this.keyWordList.add(word);
+			}
+		}
+	}
+	
+	/**
+	 * TODO: 테스트 필요
+	 */
+	private boolean isSpecialChar(String str) {
 		char c;
 		int cint;
 		for (int n = 0; n < str.length(); n++) {
 			c = str.charAt(n);
 			cint = (int)c;
 			if (cint < 48 || (cint > 57 && cint < 65) || (cint > 90 && cint < 97) || cint > 122) {
+				return false;
 			}
+		}
+		
+		return true;
+	}
+	
+	public void createSentenceList() {
+
+		List<String> sList = Lists.newArrayList(
+			Splitter.on(". ")
+				.trimResults()
+				.omitEmptyStrings()
+				.split(getContents())
+			);
+
+		for (int i = 0; i < sList.size(); i++) {
+			String sentence = sList.get(i);
+			
+			SentenceInfo scInfo = new SentenceInfo();
+			scInfo.setIndex(i);
+			scInfo.setLength(sentence.length());
+			scInfo.setSentance(sentence);
+			scInfo.checkKeyword(this.keyWordList);
+
+			this.sentenceList.add(scInfo);
 		}
 	}
 
@@ -133,6 +168,14 @@ public class Article {
 
 	public void setSentenceList(List<SentenceInfo> sentenceList) {
 		this.sentenceList = sentenceList;
+	}
+
+	public List<SentenceInfo> getKeySentenceList() {
+		return keySentenceList;
+	}
+
+	public void setKeySentenceList(List<SentenceInfo> keySentenceList) {
+		this.keySentenceList = keySentenceList;
 	}
 
 }
