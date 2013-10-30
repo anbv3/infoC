908cc7da8a44709b7c1ee1fea83189f6e3699cbc
anbv3 <anbv3@naver.com>
2013-10-31 02:17:06 +0900

    naver news 도 리팩토링..

--- src/main/java/com/infoc/controller/HomeController.java
+++ src/main/java/com/infoc/controller/HomeController.java
@@ -4,17 +4,22 @@ import java.util.List;
 
 import org.joda.time.DateTime;
 import org.joda.time.DateTimeZone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.infoc.crawler.DaumNewsCrawler;
+import com.infoc.crawler.NaverNewsCrawler;
 import com.infoc.domain.Article;
+import com.infoc.rss.Nnews;
 import com.infoc.service.CollectionService;
 import com.infoc.service.ContentsAnalysisService;
 
 @Controller
 public class HomeController extends BaseController {
+	private static final Logger LOG = LoggerFactory.getLogger(HomeController.class);
 
 	@RequestMapping(value = {"/", "/home"})
 	public String home(Model model) {
@@ -23,13 +28,15 @@ public class HomeController extends BaseController {
 		DaumNewsCrawler d = new DaumNewsCrawler();
 		List<Article> list = d.createArticlList();
 
+		NaverNewsCrawler n = new NaverNewsCrawler();
+		list.addAll(n.createArticlList());
+		
 		for (Article article : list) {
 			// create the main contents
 			ContentsAnalysisService.createMainSentence(article);
 			
 			// add to the store
 			CollectionService.add(article);
-			
 		}
 		/////////////////////////////////////////////////////////////
 		
--- src/main/java/com/infoc/domain/Article.java
+++ src/main/java/com/infoc/domain/Article.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.aspectj.apache.bcel.generic.IINC;
 import org.joda.time.DateTime;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -20,7 +21,7 @@ import com.google.common.collect.Ordering;
  */
 public class Article {
 	private static final Logger LOG = LoggerFactory.getLogger(Article.class);
-	
+
 	private String hashId; // for later
 
 	private String title;
@@ -34,15 +35,14 @@ public class Article {
 	private DateTime pubDate;
 
 	private String author;
-	
-	
-	///////////////////////////////////////////////////////////////////////////////
-	private Set<String> keyWordList = new HashSet<>(); // use for summarization and duplication check
+
+	// /////////////////////////////////////////////////////////////////////////////
+	private Set<String> keyWordList = new HashSet<>(); // use for summarization
+														// and duplication check
 
 	private String mainContents;
-	///////////////////////////////////////////////////////////////////////////////
-	
-	
+	// /////////////////////////////////////////////////////////////////////////////
+
 	public static final Ordering<Article> dateOrder = new Ordering<Article>() {
 		@Override
 		public int compare(Article left, Article right) {
@@ -63,140 +63,137 @@ public class Article {
 			return left.getIndex().compareTo(right.getIndex());
 		}
 	};
-	
-	
+
 	public void createContentsFromLink() {
 		try {
-			String contentId = "";
-			if(this.link.contains("news.naver.com")) {
-				
-				contentId = "#articleBody"; 
-				
-			} else if (this.link.contains("media.daum.net")) {
-				
-				contentId = "#newsBodyShadow";
-				
-			} else if (this.link.contains("interview365.com")) {
-				
-				contentId = "#IDContents";
-				
-			} else if (this.link.contains("www.ittoday.co.kr")) {
-				
+			String contentId = null;
+			if (this.link.contains("news.naver.com")) {
+
 				contentId = "#articleBody";
-				
+
+			} else if (this.link.contains("media.daum.net")) {
+
+				contentId = "#newsBodyShadow";
+
+			} else if (this.link.contains("interview365.com")) {
+
+				contentId = "#IDContents";
+
+			} else if (this.link.contains("www.ittoday.co.kr")) {
+
+				contentId = "#articleBody";
+
 			} else if (this.link.contains("www.sportsworldi.com")) {
-				
+
 				contentId = "#article_content";
-				
-			} else if (this.link.contains("tvdaily.mk.co.kr") || 
-				this.link.contains("etoday.co.kr") ||
-				this.link.contains("vop.co.kr") ) {
-				
+
+			} else if (this.link.contains("tvdaily.mk.co.kr")
+					|| this.link.contains("etoday.co.kr")
+					|| this.link.contains("vop.co.kr")) {
+
 				contentId = "#CmAdContent";
-				
+
 			} else {
 				return;
 			}
-			
+
 			Document doc = Jsoup.connect(this.link).get();
 			Elements newsHeadlines = doc.select(contentId);
-			
 			this.contents = newsHeadlines.text();
+
 		} catch (IOException e) {
 			LOG.error("", e);
 		}
 	}
 
-//	public void createKeyWorkList() {
-//		if (Strings.isNullOrEmpty(this.title)) {
-//			return;
-//		}
-//
-//		// eliminate special characters from title and split it
-//		Set<String> titleList = Sets.newHashSet(CollectionService.SPLITTER
-//				.split(this.title.replaceAll("[^\\p{L}\\p{Z}]", "")));
-//		
-//		for (String word : titleList) {
-//			if (word.length() > 1 && !isSpecialChar(word)) {
-//				this.keyWordList.add(word);
-//			}
-//		}
-//	}
-//
-//	/**
-//	 * TODO: 테스트 필요
-//	 */
-//	private boolean isSpecialChar(String str) {
-//		char c;
-//		int cint;
-//		for (int n = 0; n < str.length(); n++) {
-//			c = str.charAt(n);
-//			cint = (int) c;
-//			if (cint < 48 || (cint > 57 && cint < 65)
-//					|| (cint > 90 && cint < 97) || cint > 122) {
-//				return false;
-//			}
-//		}
-//
-//		return true;
-//	}
-//
-//	public void extractMainContents() {
-//		createSentenceList();
-//		createKeySentenceList();
-//		
-//		StringBuilder sb = new StringBuilder();
-//		for(SentenceInfo sentence : this.keySentenceList) {
-//			
-//			sb.append(sentence.getSentance()).append(". ");
-//		}
-//		
-//		this.contents = sb.toString();
-//	}
-//
-//	public void createSentenceList() {
-//
-//		List<String> sList = Lists.newArrayList(Splitter.onPattern("\\.\\s").trimResults()
-//				.omitEmptyStrings().split(getContents()));
-//
-//		for (int i = 0; i < sList.size(); i++) {
-//			String sentence = sList.get(i);
-//
-//			SentenceInfo scInfo = new SentenceInfo();
-//			scInfo.setIndex(i);
-//			scInfo.setLength(sentence.length());
-//			scInfo.setSentance(sentence);
-//			scInfo.checkKeyword(this.keyWordList);
-//
-//			this.sentenceList.add(scInfo);
-//		}
-//	}
-//	
-//	public void createKeySentenceList() {
-//
-//		List<SentenceInfo> matchedOrderList = Article.matchedOrder.nullsLast().reverse().sortedCopy(this.sentenceList);
-//		int maxKeySentence = matchedOrderList.size() <= 3 ? matchedOrderList.size() : 3; 
-//		
-//		for (int i = 0; i < maxKeySentence; i++) {
-//			this.keySentenceList.add(matchedOrderList.get(i));
-//		}
-//		
-//		Collections.sort(this.keySentenceList, Article.indexOrder.nullsFirst());
-//	}
+	// public void createKeyWorkList() {
+	// if (Strings.isNullOrEmpty(this.title)) {
+	// return;
+	// }
+	//
+	// // eliminate special characters from title and split it
+	// Set<String> titleList = Sets.newHashSet(CollectionService.SPLITTER
+	// .split(this.title.replaceAll("[^\\p{L}\\p{Z}]", "")));
+	//
+	// for (String word : titleList) {
+	// if (word.length() > 1 && !isSpecialChar(word)) {
+	// this.keyWordList.add(word);
+	// }
+	// }
+	// }
+	//
+	// /**
+	// * TODO: 테스트 필요
+	// */
+	// private boolean isSpecialChar(String str) {
+	// char c;
+	// int cint;
+	// for (int n = 0; n < str.length(); n++) {
+	// c = str.charAt(n);
+	// cint = (int) c;
+	// if (cint < 48 || (cint > 57 && cint < 65)
+	// || (cint > 90 && cint < 97) || cint > 122) {
+	// return false;
+	// }
+	// }
+	//
+	// return true;
+	// }
+	//
+	// public void extractMainContents() {
+	// createSentenceList();
+	// createKeySentenceList();
+	//
+	// StringBuilder sb = new StringBuilder();
+	// for(SentenceInfo sentence : this.keySentenceList) {
+	//
+	// sb.append(sentence.getSentance()).append(". ");
+	// }
+	//
+	// this.contents = sb.toString();
+	// }
+	//
+	// public void createSentenceList() {
+	//
+	// List<String> sList =
+	// Lists.newArrayList(Splitter.onPattern("\\.\\s").trimResults()
+	// .omitEmptyStrings().split(getContents()));
+	//
+	// for (int i = 0; i < sList.size(); i++) {
+	// String sentence = sList.get(i);
+	//
+	// SentenceInfo scInfo = new SentenceInfo();
+	// scInfo.setIndex(i);
+	// scInfo.setLength(sentence.length());
+	// scInfo.setSentance(sentence);
+	// scInfo.checkKeyword(this.keyWordList);
+	//
+	// this.sentenceList.add(scInfo);
+	// }
+	// }
+	//
+	// public void createKeySentenceList() {
+	//
+	// List<SentenceInfo> matchedOrderList =
+	// Article.matchedOrder.nullsLast().reverse().sortedCopy(this.sentenceList);
+	// int maxKeySentence = matchedOrderList.size() <= 3 ?
+	// matchedOrderList.size() : 3;
+	//
+	// for (int i = 0; i < maxKeySentence; i++) {
+	// this.keySentenceList.add(matchedOrderList.get(i));
+	// }
+	//
+	// Collections.sort(this.keySentenceList, Article.indexOrder.nullsFirst());
+	// }
 
-	
 	@Override
 	public String toString() {
-		return Objects.toStringHelper(this)
-			.add("title", this.title)
-			.add("link", this.link)
-			.add("pubDate", this.pubDate)
-			.add("mainContents", this.mainContents)
-			.add("keyWordList", this.keyWordList)
-			.toString();
+		return Objects.toStringHelper(this).add("title", this.title)
+				.add("link", this.link).add("pubDate", this.pubDate)
+				.add("mainContents", this.mainContents)
+				.add("keyWordList", this.keyWordList).toString();
 	}
-	
-	
+
 	public String getHashId() {
 		return hashId;
 	}
@@ -261,21 +258,21 @@ public class Article {
 		this.keyWordList = keyWordList;
 	}
 
-//	public List<SentenceInfo> getSentenceList() {
-//		return sentenceList;
-//	}
-//
-//	public void setSentenceList(List<SentenceInfo> sentenceList) {
-//		this.sentenceList = sentenceList;
-//	}
-//
-//	public List<SentenceInfo> getKeySentenceList() {
-//		return keySentenceList;
-//	}
-//
-//	public void setKeySentenceList(List<SentenceInfo> keySentenceList) {
-//		this.keySentenceList = keySentenceList;
-//	}
+	// public List<SentenceInfo> getSentenceList() {
+	// return sentenceList;
+	// }
+	//
+	// public void setSentenceList(List<SentenceInfo> sentenceList) {
+	// this.sentenceList = sentenceList;
+	// }
+	//
+	// public List<SentenceInfo> getKeySentenceList() {
+	// return keySentenceList;
+	// }
+	//
+	// public void setKeySentenceList(List<SentenceInfo> keySentenceList) {
+	// this.keySentenceList = keySentenceList;
+	// }
 
 	public String getMainContents() {
 		return mainContents;
