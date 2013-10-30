6e5f55b50e0298fbdd4d2de66a7c0676d24c9499
anbv3 <anbv3@naver.com>
2013-10-31 02:17:06 +0900

    naver news 도 리팩토링..

--- src/main/java/com/infoc/crawler/NaverNewsCrawler.java
+++ src/main/java/com/infoc/crawler/NaverNewsCrawler.java
@@ -20,57 +20,69 @@ import com.sun.syndication.feed.synd.SyndEntry;
 /**
  * @author NBP
  */
-public class DaumNewsCrawler implements NewsCrawler {
-	private static final Logger LOG = LoggerFactory.getLogger(DaumNewsCrawler.class);
-	private static String RSS_URL = "http://media.daum.net/syndication/today_sisa.rss"; // 주요기사
+public class NaverNewsCrawler implements NewsCrawler {
+	private static final Logger LOG = LoggerFactory
+			.getLogger(NaverNewsCrawler.class);
+	private static String RSS_URL = "http://news.search.naver.com/newscluster/rss.nhn?type=0&rss_idx=2";
 
 	@Override
 	public List<Article> createArticlList() {
 		List<Article> articleList = new ArrayList<>();
 		List<SyndEntry> rssList = RSSCrawler.getArticleList(RSS_URL);
 
-		LOG.debug("D News size: {}", rssList.size());
+		LOG.debug("Naver News size: {}", rssList.size());
 
 		for (SyndEntry item : rssList) {
-			articleList.add(parseRSSItem(item));
+			Article article = parseRSSItem(item);
+			if (article == null) {
+				continue;
+			}
+
+			articleList.add(article);
 		}
 
 		return articleList;
 	}
 
 	private Article parseRSSItem(SyndEntry rssItem) {
+		// Skip the news written in only English.
+		if (!rssItem.getTitle().matches(".*[ㄱ-ㅎㅏ-ㅣ가-힣]+.*")) {
+			return null;
+		}
+
 		Article article = new Article();
 		article.setAuthor(rssItem.getAuthor());
 		article.setLink(rssItem.getLink());
-		article.setPubDate(new DateTime(rssItem.getPublishedDate(), DateTimeZone.forID("Asia/Seoul")));
+		article.setPubDate(new DateTime(rssItem.getPublishedDate(),
+				DateTimeZone.forID("Asia/Seoul")));
 		article.setTitle(rssItem.getTitle());
-
+		
 		article.createContentsFromLink();
 		if (Strings.isNullOrEmpty(article.getContents())) {
 			article.setContents(rssItem.getDescription().getValue());
 		}
-		
+
 		clearInvalidWords(article);
 
 		return article;
 	}
 
 	/**
-	 * extract some text enclosed by special characters
-	 * each article has different extraction rules. 
+	 * extract some text enclosed by special characters each article has
+	 * different extraction rules.
 	 */
 	public void clearInvalidWords(Article article) {
-		String desc = article.getContents();
+		String desc = article.getContents().replaceAll("&nbsp;", "");
 
 		StringBuffer sb = new StringBuffer();
 		boolean appendFlag = true;
 		for (int i = 0; i < desc.length(); i++) {
 			char c = desc.charAt(i);
-			if (c == '[' || c == '【' || c == '(') {
+			if (c == '【') {
 				appendFlag = false;
 				continue;
 			}
-			if (c == ']' || c == '】' || c == ')') {
+			if (c == '】') {
 				appendFlag = true;
 				continue;
 			}
