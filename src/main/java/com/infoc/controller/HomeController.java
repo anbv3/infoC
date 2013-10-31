fd46585e24db9f73e5f4b3fce364407f7fd42894
anbv3 <anbv3@naver.com>
2013-10-31 16:40:34 +0900

    스케줄러 구현중..

--- src/main/java/com/infoc/controller/HomeController.java
+++ src/main/java/com/infoc/controller/HomeController.java
@@ -9,11 +9,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.infoc.crawler.DaumNewsCrawler;
 import com.infoc.crawler.NaverNewsCrawler;
 import com.infoc.domain.Article;
-import com.infoc.rss.Nnews;
 import com.infoc.service.CollectionService;
 import com.infoc.service.ContentsAnalysisService;
 
@@ -24,27 +24,31 @@ public class HomeController extends BaseController {
 	@RequestMapping(value = {"/", "/home"})
 	public String home(Model model) {
 
-		// TODO: need to run by timer separately ///////////////////////////////
-		DaumNewsCrawler d = new DaumNewsCrawler();
-		List<Article> list = d.createArticlList();
-
-		NaverNewsCrawler n = new NaverNewsCrawler();
-		list.addAll(n.createArticlList());
-		
-		for (Article article : list) {
-			// create the main contents
-			ContentsAnalysisService.createMainSentence(article);
-			
-			// add to the store
-			CollectionService.add(article);
-		}
-		/////////////////////////////////////////////////////////////
-		
-		
 		model.addAttribute("articleMap", CollectionService.CACHE);
 		model.addAttribute("currentHour", DateTime.now(DateTimeZone.forID("Asia/Seoul")).getHourOfDay());
 
 		return "/home";
 	}
 
+	@RequestMapping(value = "/crawl")
+	@ResponseBody
+	public String crawl() {
+		LOG.info("{}", DateTime.now(DateTimeZone.forID("Asia/Seoul")));
+		
+		DaumNewsCrawler d = new DaumNewsCrawler();
+		List<Article> list = d.createArticlList();
+
+		NaverNewsCrawler n = new NaverNewsCrawler();
+		list.addAll(n.createArticlList());
+
+		for (Article article : list) {
+			// create the main contents
+			ContentsAnalysisService.createMainSentence(article);
+
+			// add to the store
+			CollectionService.add(article);
+		}
+
+		return "success";
+	}
 }
