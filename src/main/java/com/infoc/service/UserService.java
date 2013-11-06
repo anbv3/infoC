b48b6333cad0a027a9724502ba76ac74c7a35533
anbv3 <anbv3@naver.com>
2013-11-07 01:24:52 +0900

    google news 추가

--- src/main/java/com/infoc/service/UserService.java
+++ src/main/java/com/infoc/service/UserService.java
@@ -1,13 +1,7 @@
 package com.infoc.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import com.infoc.domain.User;
-import com.infoc.repository.UserRepository;
 
 @Service
 @Transactional(readOnly=true)
