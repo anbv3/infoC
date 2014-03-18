24c98d5cd34b64cd0328b5085f1f40956315b953
NB10999 <sanghun.an@nhn.com>
2014-03-18 20:55:27 +0900

    테스트 코드 추가

---
+++ src/main/java/com/infoc/service/CollectionService2.java
@@ -0,0 +1,16 @@
+package com.infoc.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.infoc.domain.Article;
+
+@Service
+public class CollectionService2 {
+	private static final Logger LOG = LoggerFactory.getLogger(CollectionService2.class);
+	
+	public void add(Article newArticle) {
+		
+	}
+}
