4e36cb7f98d9879a600fbe5d3cb4bedda8718031
anbv3 <anbv333@gmail.com>
2014-06-09 20:11:49 +0900

    파싱 케이스 추가

--- src/test/java/com/infoc/util/RSSCrawlerTest.java
+++ src/test/java/com/infoc/util/RSSCrawlerTest.java
@@ -7,6 +7,7 @@ import com.sun.syndication.io.SyndFeedInput;
 import com.sun.syndication.io.XmlReader;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.slf4j.Logger;
@@ -59,16 +60,64 @@ public class RSSCrawlerTest {
 		}
 	}
 
+
+    @Test
+    public void testLATimes() {
+        String url = "http://www.nytimes.com/2014/06/08/us/though-not-quietly-kentucky-moves-to-cut-reliance-on-coal.html";
+
+        Document doc;
+        try {
+
+            doc = Jsoup.connect(url)
+                       .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                       .timeout(6000)
+                       .get();
+
+            LOG.debug("{}", doc.toString());
+        }
+        catch (IOException e) {
+            LOG.debug("", e);
+        }
+
+    }
+
 	@Test
-	public void testLATimes() {
-		String url = "http://feedproxy.google.com/~r/ppss/~3/AvRL5mGWTNw/21344";
+	public void testNYTimes() {
+		String url = "http://rss.nytimes.com/c/34625/f/640372/s/3b466d67/sc/7/l/0L0Snytimes0N0C20A140C0A60C0A80Cus0Cthough0Enot0Equietly0Ekentucky0Emoves0Eto0Ecut0Ereliance0Eon0Ecoal0Bhtml0Dpartner0Frss0Gemc0Frss/story01.htm";
 
 		Document doc;
 		try {
 
-			doc = Jsoup.connect(url).userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0").timeout(6000).get();
-			Elements contentsArea = doc.select(".tha-content");
-			LOG.debug("{}", contentsArea.text().replaceAll( "([\\ud800-\\udbff\\udc00-\\udfff])", ""));
+			doc = Jsoup.connect(url)
+                       .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                       .timeout(6000)
+                       .get();
+
+            //LOG.debug("{}", doc.toString());
+
+			Elements contentsArea = doc.select(".medium a");
+            LOG.debug("{}", contentsArea.html());
+            LOG.debug("{}", contentsArea.attr("href"));
+
+            url = contentsArea.attr("href");
+            doc = Jsoup.connect(url)
+                       .userAgent("Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0")
+                       .timeout(6000)
+                       .get();
+            LOG.debug("{}", doc.toString());
+
+            String contentId = "#story";
+            contentsArea = doc.select(contentId);
+
+            Elements bodyArea = contentsArea.select(".story-body-text");
+            StringBuilder sb = new StringBuilder();
+            for (Element element : bodyArea) {
+                sb.append(element.ownText()).append(" ");
+            }
+
+            LOG.debug("{}", sb.toString());
+
+//			LOG.debug("{}", contentsArea.text().replaceAll( "([\\ud800-\\udbff\\udc00-\\udfff])", ""));
 
 			//String img = contentsArea.select("img").attr("src");
 			//LOG.debug("{}", img);
