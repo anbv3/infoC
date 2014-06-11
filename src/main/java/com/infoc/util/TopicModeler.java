52bbaa7f8c1a793b26e035e44654ef0ae1b2a3f7
anbv3 <anbv333@gmail.com>
2014-06-11 13:51:52 +0900

    메모리 때문에 모델러 사용 로직 변경

--- src/main/java/com/infoc/util/TopicModeler.java
+++ src/main/java/com/infoc/util/TopicModeler.java
@@ -31,43 +31,40 @@ public class TopicModeler {
     private static final Logger LOG = LoggerFactory.getLogger(TopicModeler.class);
 
     private static final int NUM_ITERATIONS = 50;
-    private static final int NUM_TOPICS = 6;
+    private static final int NUM_TOPIC = 1;
+    private static final int NUM_TOPIC_WORDS = 6;
+
+    // Begin by importing documents from text to feature sequences
+    private static ArrayList<Pipe> PIPE_LIST = new ArrayList<>();
+
+    static {
+        // Pipes: lowercase, tokenize, remove stopwords, map to features
+        PIPE_LIST.add(new CharSequenceLowercase());
+        PIPE_LIST.add(new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")));
+
+        File file = null;
+        try {
+            file = new ClassPathResource("stoplists/en.txt").getFile();
+        } catch (IOException e) {
+        }
+        PIPE_LIST.add(new TokenSequenceRemoveStopwords(file, "UTF-8", false, false, false));
+        PIPE_LIST.add(new TokenSequence2FeatureSequence());
+    }
+
 
     public static synchronized Set<String> getMainTopics(String contents) throws IOException {
-
-        // Begin by importing documents from text to feature sequences
-        ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
-
-        // Pipes: lowercase, tokenize, remove stopwords, map to features
-        pipeList.add(new CharSequenceLowercase());
-        pipeList.add(new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")));
-
-        File file = new ClassPathResource("stoplists/en.txt").getFile();
-        pipeList.add(new TokenSequenceRemoveStopwords(file, "UTF-8", false, false, false));
-        pipeList.add(new TokenSequence2FeatureSequence());
-
-        InstanceList instances = new InstanceList(new SerialPipes(pipeList));
         Reader fileReader = new InputStreamReader(new ByteArrayInputStream(contents.getBytes()));
-        instances.addThruPipe(new CsvIterator(fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$"), 3, 2, 1)); // data, label, name fields
 
-        // Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
-        //  Note that the first parameter is passed as the sum over topics, while
-        //  the second is the parameter for a single dimension of the Dirichlet prior.
-        int numTopics = 1;
-        ParallelTopicModel model = new ParallelTopicModel(numTopics, 1.0, 0.01);
+        InstanceList instances = new InstanceList(new SerialPipes(PIPE_LIST));
+        instances.addThruPipe(new CsvIterator(fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$"), 3, 2, 1));
 
+        ParallelTopicModel model = new ParallelTopicModel(NUM_TOPIC, 1.0, 0.01);
         model.addInstances(instances);
 
-        // Use two parallel samplers, which each look at one half the corpus and combine
-        //  statistics after every iteration.
-        model.setNumThreads(1);
-
-        // Run the model for 50 iterations and stop (this is for testing only,
-        //  for real applications, use 1000 to 2000 iterations)
+        model.setNumThreads(2);
         model.setNumIterations(NUM_ITERATIONS);
-        model.estimate();
 
-        // Show the words and topics in the first instance
+        model.estimate();
 
         // The data alphabet maps word IDs to strings
         Alphabet dataAlphabet = instances.getDataAlphabet();
@@ -78,22 +75,25 @@ public class TopicModeler {
         Set<String> keywordList = new HashSet<>();
 
         // Show top 5 words in topics with proportions for the first document
-        for (int topic = 0; topic < numTopics; topic++) {
+        for (int topic = 0; topic < NUM_TOPIC; topic++) {
             Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
 
             int rank = 0;
-            while (iterator.hasNext() && rank < NUM_TOPICS) {
+            while (iterator.hasNext() && rank < NUM_TOPIC_WORDS) {
                 IDSorter idCountPair = iterator.next();
-                String topicWord = (String)dataAlphabet.lookupObject(idCountPair.getID());
+                String topicWord = (String) dataAlphabet.lookupObject(idCountPair.getID());
                 if (topicWord.isEmpty()) {
                     continue;
                 }
                 keywordList.add(topicWord);
-                
+
                 rank++;
             }
         }
-        
+
+        fileReader.close();
+        instances.removeSources();
+        instances = null;
         return keywordList;
     }
 }
