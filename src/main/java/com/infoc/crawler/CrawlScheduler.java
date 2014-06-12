a784128f32ab4c5b0e124dc5d5bb9c76666c93d3
anbv3 <anbv3@naver.com>
2014-06-13 00:02:02 +0900

    웁스.. 스케줄..

--- src/main/java/com/infoc/crawler/CrawlScheduler.java
+++ src/main/java/com/infoc/crawler/CrawlScheduler.java
@@ -69,21 +69,20 @@ public class CrawlScheduler {
 	
 	private void setUpCrawlerList() {
 		// kr
-//		newsCrawlerList.add(daumNewsCrawler);
-//		newsCrawlerList.add(naverNewsCrawler);
-//		newsCrawlerList.add(googleNewsCrawler);
-//		newsCrawlerList.add(otherNewsCrawler);
-//
-//		// us
-//
-//        // impossible to parse RSS
-//        // newsCrawlerList.add(nyTimesCrawler);
-//        // newsCrawlerList.add(bostonNewsCrawler);
-//		//newsCrawlerList.add(chicagoTribuneCrawler);
-//
-//		newsCrawlerList.add(timeCrawler);
-//		newsCrawlerList.add(laTimesCrawler);
-//		newsCrawlerList.add(baseballNewsCrawler);
+		newsCrawlerList.add(daumNewsCrawler);
+		newsCrawlerList.add(naverNewsCrawler);
+		newsCrawlerList.add(googleNewsCrawler);
+		newsCrawlerList.add(otherNewsCrawler);
+
+        // impossible to parse RSS
+        // newsCrawlerList.add(nyTimesCrawler);
+        // newsCrawlerList.add(bostonNewsCrawler);
+		// newsCrawlerList.add(chicagoTribuneCrawler);
+
+		// us
+		newsCrawlerList.add(timeCrawler);
+		newsCrawlerList.add(laTimesCrawler);
+		newsCrawlerList.add(baseballNewsCrawler);
 	}
 
 	private void setUpSchedules() {
